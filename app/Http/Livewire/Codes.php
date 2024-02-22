f71efc0fcd600e86f95485bb4c5a7af3afe1208f
Alfredo Gonzalez <alfredomarenco@boletea.com>
2024-02-22 12:59:31 -0600

    Avances en apartado base de datos

--- app/Http/Livewire/Codes.php
+++ app/Http/Livewire/Codes.php
@@ -4,21 +4,33 @@ namespace App\Http\Livewire;
 
 use App\Models\Event;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Codes extends Component
 {
+    use WithPagination;
+
+    public $search='';
+    public $actionSelect = 1;
     public $event;
+
+    /* public function updatingSearch()
+    {
+        $this->resetPage();
+    } */
 
     public function mount (Event $event){
         $this->event = $event;
     }
 
-
+    public function selectAction($value){
+        $this->actionSelect = $value;
+    }
 
     public function render()
     {
         return view('livewire.codes',[
-            'codes' => $this->event->codes()->paginate(10)
+            'codes' => $this->event->codes()->where('barcode','LIKE','%'.$this->search.'%')->orderBy('barcode', 'asc')->paginate(10)
         ]);
     }
 }
--- resources/views/livewire/codes.blade.php
+++ resources/views/livewire/codes.blade.php
@@ -5,9 +5,84 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-4">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg mb-1 p-3">
+                <div class="border-b border-gray-200 pb-5 sm:pb-0">
+                    <h3 class="text-base font-semibold leading-6 text-gray-900 mb-2">Acctions</h3>
+                    {{-- <div class="mt-3 sm:mt-4">
+                        <!-- Dropdown menu on small screens -->
+                        <div class="sm:hidden">
+                            <label for="current-tab" class="sr-only">Select a tab</label>
+                            <select id="current-tab" name="current-tab"
+                                class="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
+                                <option>Search</option>
+                                <option>Add</option>
+                                <option selected>Interview</option>
+                                <option>Offer</option>
+                                <option>Hired</option>
+                            </select>
+                        </div> --}}
+                    <!-- Tabs at small breakpoint and up -->
+                    <div class="hidden sm:block">
+                        <nav class="-mb-px flex space-x-8">
+                            <!-- Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
+                            <a wire:click="selectAction('1')"
+                                class="flex cursor-pointer items-center border-transparent {{ $actionSelect == '1' ? 'border-indigo-500 text-indigo-600' : 'text-gray-500 hover:border-gray-300 hover:text-gray-700' }} whitespace-nowrap border-b-2 px-1 pb-4 text-sm font-medium"><svg
+                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                                </svg>
+                                Search</a>
+                            <a wire:click="selectAction('2')"
+                                class="flex cursor-pointer items-center border-transparent {{ $actionSelect == '2' ? 'border-indigo-500 text-indigo-600' : 'text-gray-500 hover:border-gray-300 hover:text-gray-700' }} whitespace-nowrap border-b-2 px-1 pb-4 text-sm font-medium"><svg
+                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                </svg>
+                                Add</a>
+                            <a wire:click="selectAction('3')"
+                                class="flex cursor-pointer items-center {{ $actionSelect == '3' ? 'border-indigo-500 text-indigo-600' : 'text-gray-500 hover:border-gray-300 hover:text-gray-700' }} whitespace-nowrap border-b-2 px-1 pb-4 text-sm font-medium"
+                                aria-current="page"><svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M12 9.75v6.75m0 0-3-3m3 3 3-3m-8.25 6a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
+                                </svg>
+                                Import</a>
+                            <a wire:click="selectAction('4')"
+                                class="flex cursor-pointer items-center border-transparent {{ $actionSelect == '4' ? 'border-indigo-500 text-indigo-600' : 'text-gray-500 hover:border-gray-300 hover:text-gray-700' }} whitespace-nowrap border-b-2 px-1 pb-4 text-sm font-medium"><svg
+                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
+                                </svg>
+
+                                Export</a>
+                            <a wire:click="selectAction('5')"
+                                class="flex cursor-pointer items-center border-transparent {{ $actionSelect == '5' ? 'border-indigo-500 text-indigo-600' : 'text-gray-500 hover:border-gray-300 hover:text-gray-700' }} whitespace-nowrap border-b-2 px-1 pb-4 text-sm font-medium"><svg
+                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 48.678 48.678 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 0 0 3.7 3.7 48.656 48.656 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3-3 3" />
+                                </svg>
+                                Clear</a>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @if ($actionSelect == 1)
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="relative mb-2">
+                <x-input wire:model="search" type="text" value="{{ $search }}" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></x-input>
+            </div>
+            {{ $search }}
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="p-3">
+                    {{ $codes->links() }}
+                </div>
                 <ul role="list" class="divide-y divide-gray-100 p-3">
                     @foreach ($codes as $code)
                         <li class="flex justify-between gap-x-6 py-5">
@@ -64,5 +139,6 @@
                 </div>
             </div>
         </div>
-    </div>
+    @endif
+</div>
 </div>
